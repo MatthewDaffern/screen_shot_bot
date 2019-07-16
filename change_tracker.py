eb147dfc3c7cbd12de08243c01a74195260543f3
MatthewDaffern <38901216+MatthewDaffern@users.noreply.github.com>
2019-07-15 17:05:24 -0700

    clarified some things with the magic of dict

--- change_tracker.py
+++ change_tracker.py
@@ -5,26 +5,31 @@ import json
 import ssl
 import smtplib
 
+
 #good
 def digest_returner(object_input):
     hash_object = sha3_512(str.encode(object_input))
     return hash_object.hexdigest()
+
 #good
 def website_text_getter(str_input):
     website = requests.get(str_input)
     return website.text
+
 #good
 def checker(hash_one, hash_two):
     if hash_one == hash_two:
         return False
     else:
         return True
+
 #good
 def fold(func_list, input_object):
     variable_holder = str()
     for i in func_list:
         variable_holder = i(input_object)
     return variable_holder
+
 
 #good
 def website_from_config(file_name_import):
@@ -40,25 +45,33 @@ def hash_to_check(file_name_import):
     return loaded_dict['website_hash']
 
 
-
+#good
+def config_grabber(file_name_import):
+    json_file = open(file_name_import, "r+")
+    loaded_dict = json.load(json_file)
+    return loaded_dict
 
 def checking_function(file_name_import):
     hash_check = partial(checker, hash_two=hash_to_check(file_name_import))
     func_list = [website_text_getter, digest_returner, hash_check]
-    is_changed = fold(func_list, website_from_config('config.json'))
+    website = website_from_config('config.json')
+    is_changed = fold(func_list, website)
     if is_changed == True:
-        return True
+        return website
+
+def none_type_try_catch(check_input, function):
+    if type(check_input) == None:
+        return None
     else:
-        return False
-
-
+        function(check_input)
 
 
 #good
-def server_context(port, address):
+def server_context(address):
     context = ssl.create_default_context()
-    return smtplib.SMTP_SSL(address, port, context=context)
+    return smtplib.SMTP_SSL(address[address], address[port], context=context)
 
+#good
 def login(server_input, creds):
     return server_input.login(creds[username], creds[password])
 
@@ -67,16 +80,17 @@ def message(server_input, mail_message):
     print('success!')
     return mail_message
 
+def message_creator(url_input):
+    return '{url_input} has changed. Please visit the page'
 
-def send_message(port_input, address_input, creds_input, message_input):
-    message_partial = partial(message, mail_message=message_input)
-    login_partial = partial(login, creds=creds_input)
-    server = login_partial(server_context(port_input, address_input))
-    return message_partial(server)
+def send_message(config_input, mail_message_input):
+    login_with_creds = partial(login, creds=config_input)
+    pre_filled_message = partial(message, mail_message=message_creator(mail_message_input))
+    return message(login(server_context(config_input)))
 
 def main():
-# TODO the whole thing"
-
+    configured_smtp = partial(send_message, config_input=config_grabber('email.json'))
+    none_type_try_catch(checking_function('config.json'), message_creator)
 
 if __name__=="__main__":
     main()
--- email_config.py
+++ email_config.py
@@ -7,15 +7,22 @@ def password():
 def username():
     return input('please enter your username \n')
 
-def dump_config(username_input, password_input):
-    cred_dict =dict([('username_field', username_input), ('password_field', password_input)])
+def server_name():
+    return input('What is the name of the mail server? \n')
+
+def port():
+    return input('What port are you using? \n')
+
+
+def dump_config(username_input, password_input, server_name_input, port_input):
+    cred_dict =dict([('username_field', username_input), ('password_field', password_input), ('server_name', server_name_input), ('port', port_input)])
     email_config_json = open('email.json', "w+")
     json.dump(cred_dict, email_config_json)
     email_config_json.close()
     print("file created")
 
 def main():
-    dump_config(username(), password())
+    dump_config(username(), password(), server_name(), port())
 
 
 if __name__=="__main__":
